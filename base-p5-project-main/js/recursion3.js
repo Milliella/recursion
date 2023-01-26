5cb9446039003130878eb7b166f60a2eb61cd5c1
Milliella <115160750+Milliella@users.noreply.github.com>
2023-01-26 10:57:51 +0000

    26/1 morning

--- base-p5-project-main/js/recursion3.js
+++ base-p5-project-main/js/recursion3.js
@@ -19,28 +19,28 @@ function draw() {
     background(220);
     resetMatrix(); //re-sets the functions each time the slider is moved
     translate(width / 2, height);
-    branch(350); //branch or tree height, you need to change it in the stroke weight map as well, the higher the starting number the larger the tree, the lsrger number of branches you have the more recursion is going to happen, the lower the amount that you times it by then the shorter its going to be
+    branch(200); //branch or tree height, you need to change it in the stroke weight map as well, the higher the starting number the larger the tree, the lsrger number of branches you have the more recursion is going to happen, the lower the amount that you times it by then the shorter its going to be
 }
 function branch(len) { //length of branch to draw
     let maxAngle = slider.value(); //smaller angles at the left hand side, larger max angle at right hand side, the max angle is dependednt on the position of the slieer and its corresponding value
-    strokeWeight(map(len, 20, 350, 1, 10));
+    strokeWeight(map(len, 20, 200, 1, 10));
     stroke(lerpColor(brown1, brown2, random(0.4, 0.6))); //random mix point vairation, lerp = one value and another, having a random value between them you're finding a random mix point for the colours 
     line(0, 0, 0, -len);
     translate(0, -len);
-    if (len > 20) { //do smothing
-        if (len < 50) { //dont
+    if (len > 20) { //do something
+        if (len < 50) { //dont do something - explaining the use of the two statements 
             //leaves
             let r = 200 + random(-20, 20); //from 180 to 220 (taking 20 away from 200 and adding 20 to 200), it means plus a number between -20 and 20, adding it to 200
             let g = 100 + random(-20, 20);
             let b = 40 + random(-20, 20);
-            fill(r, g, b, 100); // add (,100) makes it transparent
+            fill(r, g, b); // add (,100) makes it transparent
            
             //noStroke();
             strokeWeight(1); //size of outline around shapes 
             //triangle(-size/2, 0, size/2, 0, 0, -size); //the three ponits for the triangle, bottom left, middle, bottom right
-            if(random(1)> 0.95){
+            if(random(1)> 0.9){
                 for (let i = 0; i < 360; i += 36) {
-                    let size = random(15, 100);
+                    let size = (15, 20);
                     ellipse(cos(i) * size, sin(i) * size, size);
     
                 }
@@ -56,27 +56,27 @@ function branch(len) { //length of branch to draw
             //branch 2
             push();
             rotate(random(-maxAngle, maxAngle));
-            branch(len * 0.6);
+            branch(len * 0.3);
             pop();
             //branch 3
             push();
             rotate(random(-maxAngle, maxAngle));
-            branch(len * 0.8);
+            branch(len * 0.4);
             pop();
             //branch 4
             push();
             rotate(random(-maxAngle, maxAngle));
-            branch(len * 0.1);
+            branch(len * 0.5);
             pop();
             //branch 5
             push();
             rotate(random(-maxAngle, maxAngle));
-            branch(len * 0.5);
+            branch(len * 0.6);
             pop();
             //branch 6
             push();
             rotate(random(-maxAngle, maxAngle));
-            branch(len * 0.2);
+            branch(len * 0.9);
             pop();
         }
     }
